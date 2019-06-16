f8325a7dafeb39bf0d2aa7995e39192021d67100
Mathiew <amateo1982@yahoo.es>
2019-06-16 23:05:53 +0200

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,79 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div class="home">
+    <p>
+      <b>A date:</b> {{ date }}
+    </p>
+    <p>
+      <b>How long ago?</b> {{ howLong }}
+    </p>
+    <p>
+      <b>A year:</b> {{ year }}
+    </p>
+    <p>
+      <b>A month:</b> {{ month }}
+    </p>
+    <p>
+      <b>A month in text:</b> {{ monthText }}
+    </p>
+    <p>
+      <b>A day:</b> {{ day }}
+    </p>
+    <p>
+      <b>A day in text:</b> {{ dayText }}
+    </p>
+    <p>
+      <b>The time:</b> {{ time }}
+    </p>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      // date received 3 params: (Date, date or timestamp), [format, separator]
+      date: this.$dn.date(new Date(), 'dd-mm-yyyy', '-'),
+      // date: this.$dn.date('2016-04-22', 'dd-mm-yyyy', '-'),
+      // date: this.$dn.date('2014-04-22 22:56:47', 'dd-mm-yyyy', '-'),
+
+      // howLong received 1 params: Date, date or timestamp
+      howLong: this.$dn.howLong(new Date('2019-04-08'), 'en'),
+      // howLong: this.$dn.howLong('2016-04-22', 'es'),
+      // howLong: this.$dn.howLong('2019-06-12 10:56:47', 'es'),
+
+      // year received 1 params: [Date, date or timestamp]
+      year: this.$dn.year(new Date()),
+      // year: this.$dn.year('2016-04-22'),
+      // year: this.$dn.year('2014-04-22 22:56:47'),
+      // year: this.$dn.year()
+
+      // month received 1 params: [Date, date or timestamp]
+      month: this.$dn.month(new Date()),
+      // month: this.$dn.month('2016-04-22'),
+      // month: this.$dn.month('2014-04-22 22:56:47'),
+      // month: this.$dn.month()
+
+      // monthText received 1 params: [Date, date or timestamp]
+      monthText: this.$dn.monthText(new Date(), 'en'),
+      // monthText: this.$dn.monthText('2016-01-22', 'es'),
+      // monthText: this.$dn.monthText('2014-03-22 22:56:47', 'es'),
+      // monthText: this.$dn.monthText('', 'es'),
+
+      // day received 1 params: [Date, date or timestamp]
+      day: this.$dn.day(new Date()),
+      // day: this.$dn.day('2016-04-22'),
+      // day: this.$dn.day('2014-04-22 22:56:47'),
+      // day: this.$dn.day()
+
+      // dayText received 1 params: [Date, date or timestamp]
+      dayText: this.$dn.dayText(new Date(), 'en'),
+      // dayText: this.$dn.dayText('2016-01-22', 'es'),
+      // dayText: this.$dn.dayText('2014-03-22 22:56:47', 'es'),
+      // dayText: this.$dn.dayText('', 'es'),
+
+      time: this.$dn.time()
     }
   }
 }
-</style>
+</script>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
-import DateNow from '../plugins/vue-date-now.js'
+import DateNow from './'
 
 Vue.use(DateNow)
 
-Vue.config.productionTip = false
+Vue.config.productionTip = true
 
 new Vue({
-  router,
-  store,
   render: h => h(App)
 }).$mount('#app')
